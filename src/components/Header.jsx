1e3b7bd24288db4e941c7a4322741abd8bff66c3
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-01 09:58:57 -0300

    Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import styles from './header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
+
+
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt="logotipo do ignite" />
       <strong>Ignite Feed</strong>
     </header>
   )
