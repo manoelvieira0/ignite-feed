0305a03f1eab876d2e216e549f82aeed902363bb
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-01 14:34:55 -0300

    Componente Sidebar
    npm i @phosphor-icons/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { Header } from "./components/Header";
+import { Post } from './Post'
 
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/SideBar";
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Manoel Vieira"
+            content="Novo post"
+          />
+          <Post
+            author="Manoel Vieira"
+            content="Novo post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,10 @@ import styles from './header.module.css'
 
 import igniteLogo from '../assets/ignite-logo.svg'
 
-
-
 export function Header() {
   return (
     <header className={styles.header}>
       <img src={igniteLogo} alt="logotipo do ignite" />
-      <strong>Ignite Feed</strong>
     </header>
   )
 }
