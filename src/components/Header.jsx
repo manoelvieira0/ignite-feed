2f7c8cc84abf91ca0dcba7932f662c71a6c97908
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-01 09:53:16 -0300

    Estilização Global

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import styles from './header.module.css'
 
 export function Header() {
   return (
-    <header  className={styles.header}>
+    <header className={styles.header}>
       <strong>Ignite Feed</strong>
     </header>
   )
