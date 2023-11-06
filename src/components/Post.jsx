bbc4f924375a8fb4dadfa4c3d2f9c6785eb4a8c5
Manoel Vieira <netinhovieira1801@gmail.com>
2023-11-06 19:59:26 -0300

    Realizando likes nos comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
 
-  const [comments, setComments] = useState(["Post muito bacana, hein?!"])
+  const [comments, setComments] = useState([])
 
   const [newCommentText, setNewCommentText] = useState('')
 
